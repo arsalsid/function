5ebae9cc7b6c689a82d4e4cf4c84fc0dfc334b83
arsalsid <arsal243@gmail.com>
2020-01-16 22:29:56 +0500

    functions

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    static void forLoopPract () {
+
+        for(int i=0; i<10; i++) {
+            System.out.println(i);
+        }
+    }
+
+
+
+    static String returnStr () {
+        return "Hammad";
+    }
+
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("hello");
+
+        System.out.println(returnStr());
+
+        forLoopPract();
+
+    }
+
+}
+
+
---
+++ src/com/company/Test.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Test {
+
+    String a = "HAmmad";
+
+    String returnStr () {
+        return a;
+    }
+}
